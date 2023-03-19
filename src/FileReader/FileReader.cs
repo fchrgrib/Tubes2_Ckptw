4ed5c32fe4372451922834518f75029ea3d83912
fchrgrib <16521492@mahasiswa.itb.ac.id>
2023-03-20 06:34:27 +0700

    fixed FileReader

--- src/FileReader/FileReader.cs
+++ src/FileReader/FileReader.cs
@@ -9,49 +9,38 @@ namespace Tubes2_Ckptw.src.FileReader
 {
     internal class FileReader
     {
-        static void Main()
-        {
-            FileReade fileReader = new FileReade("text.txt");
-
-            Debug.WriteLine(fileReader.getMapMaze().Length);
-
-
+        private string nameFile;
+        public FileReader(string nameFile) {
+            this.nameFile = nameFile;
         }
-        class FileReade
+        public char[,] getMapMaze()
         {
-            private string nameFile;
-            public FileReade(string nameFile) {
-                this.nameFile = nameFile;
-            }
 
-            public char[,] getMapMaze()
+            string mapBefSplit = "";
+
+            try
             {
-                
-                string mapBefSplit = "";
-
-                try
-                {
-                    mapBefSplit = File.ReadAllText("../../../test/" + this.nameFile);
-                }catch(FileNotFoundException e)
-                {
-                    Debug.WriteLine(e.Message);
-                }
-
-                string[] splitFirst = mapBefSplit.Split('\n');
-                char[,] result = new char[splitFirst.Length, splitFirst[0].Split(" ").Length];
-
-                for(int i = 0; i < splitFirst.Length; i++)
-                {
-                    string[] temp = splitFirst[i].Split(" ");
-                    for(int j=0;j<temp.Length; j++)
-                    {
-                        result[i,j] = temp[j].ToCharArray()[0];
-                    }
-                }
-
-
-                return result;
+                mapBefSplit = File.ReadAllText("../../../test/" + this.nameFile);
             }
+            catch (FileNotFoundException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            string[] splitFirst = mapBefSplit.Split('\n');
+            char[,] result = new char[splitFirst.Length, splitFirst[0].Split(" ").Length];
+
+            for (int i = 0; i < splitFirst.Length; i++)
+            {
+                string[] temp = splitFirst[i].Split(" ");
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    result[i, j] = temp[j].ToCharArray()[0];
+                }
+            }
+
+
+            return result;
         }
     }
 }
